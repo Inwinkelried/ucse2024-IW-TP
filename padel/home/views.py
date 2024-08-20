78d118198da430e381b069bf9d836dc2fb8df794
Santiago Inwinkelried <101613134+Inwinkelried@users.noreply.github.com>
2024-08-20 17:29:34 -0300

    add: app 'accounts'; registration.html

--- padel/home/views.py
+++ padel/home/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # Redirige a la página de inicio después del registro
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
- 
+
 @login_required
 def protected_page(request):
     return render(request, 'protected.html')
--- padel/padel/urls.py
+++ padel/padel/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', views.register, name='register'),
-    path('protected/', views.protected_page, name='protected_page'),
-    path('', views.home, name='home'),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    # path('register/', views.register, name='register'),
+    path('protected/', views.protected_page, name='protected_page'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+  
 ]
