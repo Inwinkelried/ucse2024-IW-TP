1783a3119161e10f7300ea55b3f6b92f4236b587
Santiago Inwinkelried <101613134+Inwinkelried@users.noreply.github.com>
2024-08-20 16:11:26 -0300

    fix: nueva estructura

--- padel/padel/urls.py
+++ padel/padel/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_project project.
+URL configuration for padel project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  
-from django.views.generic.base import TemplateView 
-from views import register
-from views import protected_page
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")), 
-    path("", TemplateView.as_view(template_name="home.html"), name="home"), 
-    path('register/', register, name='register'),
-    path('protected/', protected_page, name='protected'),
 ]
