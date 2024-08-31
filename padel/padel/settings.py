ad658ca865669b3d33cf7a93d7e96f28c322514c
Facundo Gabriel Schillino Gariglio <facundoschillino01@gmail.com>
2024-08-31 13:16:57 -0300

    Revert "."
    This reverts commit d6fbf587542f383c6faf28ea54065c591ed141ef.

--- padel/padel/settings.py
+++ padel/padel/settings.py
@@ -116,7 +116,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'padel/static')]
 
-
+# Media files configuration
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -160,7 +162,3 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# Media files configuration
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
